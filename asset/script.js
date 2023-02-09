2d973f9aa2f7615517032825b91a61ae1dde2eb0
minhtrong456 <94778246+mtrong100@users.noreply.github.com>
2023-02-10 06:29:55 +0700

    again

--- asset/script.js
+++ asset/script.js
@@ -9,22 +9,42 @@ const wind = document.querySelector(".wind-value");
 const humidity = document.querySelector(".humidity-value");
 const weatherImg = document.querySelector(".weather-img img");
 
+/* get data from api */
 searchBox.addEventListener("change", getWeather);
 async function getWeather(e) {
   const res = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${API_KEY}&lang=vi&units=metric`
   );
   const data = await res.json();
+  weatherData(data);
+  // console.log(data);
+}
 
+/* display your current place */
+async function display() {
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=10.75&lon=106.6667&appid=${API_KEY}&lang=vi&units=metric`
+  );
+  const data = await res.json();
+  weatherData(data);
+  console.log(data);
+}
+display();
+
+/* render data from api */
+function weatherData(data) {
   cityName.innerHTML = data.name;
   weatherDesc.innerHTML = "Thời tiết: " + data.weather[0].description;
   wind.innerHTML = `${data.wind.speed}km/h`;
   humidity.innerHTML = `${data.main.humidity}%`;
   temperature.innerHTML = Math.floor(data.main.temp) + "°C";
-  weatherImg.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
-  console.log(data);
+  weatherImg.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+  );
 }
 
+/* some style shit */
 searchBox.addEventListener("keydown", function (e) {
   if (e.target.value) {
     searchWrapper.style.borderColor = "#6a5af9";
